206e39e71ae8d987bda5d355e9e628a0fbc35e35
Nicolas Guillen <nicomounstro@gmail.com>
2021-04-30 15:06:21 +0000

    ejercicio sin terminar

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,66 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-3 d-flex flex-column justify-content-center container tablero p-2 mx-auto rounded-lg">
+			<div className="row flex-row d-flex justify-content-center">
+				<div className="m-2 texto p-2">
+					<h1>Ta Te Ti en react.js</h1>
+				</div>
+			</div>
+			<div className="row flex-row d-flex justify-content-center">
+				<div className="m-2 texto">
+					<h2>turno de {props.jugador}</h2>
+				</div>
+			</div>
+			<div className="row flex-row d-flex justify-content-center">
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas">{props.caja0}</h1>
+				</div>
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas">{props.caja1}</h1>
+				</div>
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas">{props.caja2}</h1>
+				</div>
+			</div>
+			<div className="row flex-row d-flex justify-content-center">
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas">{props.caja3}</h1>
+				</div>
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas">{props.caja4}</h1>
+				</div>
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas">{props.caja5}</h1>
+				</div>
+			</div>
+			<div className="row flex-row d-flex justify-content-center">
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas">{props.caja6}</h1>
+				</div>
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas"> {props.caja7}</h1>
+				</div>
+				<div className="m-2 cajita p-2 d-flex justify-content-center align-items-center">
+					<h1 className="marcas">{props.caja8}</h1>
+				</div>
+			</div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	jugador: PropTypes.string,
+	caja0: PropTypes.string,
+	caja1: PropTypes.string,
+	caja2: PropTypes.string,
+	caja3: PropTypes.string,
+	caja4: PropTypes.string,
+	caja5: PropTypes.string,
+	caja6: PropTypes.string,
+	caja7: PropTypes.string,
+	caja8: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,22 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 
+let cajas = ["X", "O", "X", "O", "", "", "O", "", ""];
+let jugadores = ["X", "O"];
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+	<Home
+		jugador={jugadores[1]}
+		caja0={cajas[0]}
+		caja1={cajas[1]}
+		caja2={cajas[2]}
+		caja3={cajas[3]}
+		caja4={cajas[4]}
+		caja5={cajas[5]}
+		caja6={cajas[6]}
+		caja7={cajas[7]}
+		caja8={cajas[8]}
+	/>,
+	document.querySelector("#app")
+);
